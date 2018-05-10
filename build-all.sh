299d8b8a526d6b0964c6286d527b89ed8b089147
Balazs Nadasdi <balazs.nadasdi@cheppers.com>
2018-05-10 20:00:41 +0200

    add build-all.sh

---
+++ build-all.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ARCH=(386 amd64)
+OS=(darwin linux windows)
+TARGET_DIR=bin
+
+for arch in ${ARCH[*]}; do
+  for os in ${OS[*]}; do
+    outname="${TARGET_DIR}/totp-cli-${os}-${arch}"
+
+    if [[ "${os}" == "windows" ]]; then
+      outname="${outname}.exe"
+    fi
+
+    GOOS="${os}" GOARCH="${arch}" go build -o "${outname}" .
+  done
+done
