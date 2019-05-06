f5fb24403cfa4847cb1121133591a11d4f861753
Balazs Nadasdi <balazs.nadasdi@cheppers.com>
2019-05-06 16:44:09 +0200

    Bump version

--- info/app.go
+++ info/app.go
@@ -7,4 +7,4 @@ const AppRepoOwner string = "yitsushi"
 const AppName string = "totp-cli"
 
 // AppVersion defined current version of this application
-const AppVersion string = "1.0.4"
+const AppVersion string = "1.0.5"
