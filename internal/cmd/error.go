6ba37fbf6302bc259060280a2bfe15866e35b54b
Balazs Nadasdi <efertone@pm.me>
2022-10-16 20:20:02 +0200

    fix: remove panic calls (#59)
    Closes #56

--- internal/cmd/error.go
+++ internal/cmd/error.go
@@ -10,3 +10,30 @@ type DownloadError struct {
 func (e DownloadError) Error() string {
 	return fmt.Sprintf("download error: %s", e.Message)
 }
+
+// ImportError is an error during a file import.
+type ImportError struct {
+	Message string
+}
+
+func (e ImportError) Error() string {
+	return fmt.Sprintf("import error: %s", e.Message)
+}
+
+// GenerateError is an error during code generation.
+type GenerateError struct {
+	Message string
+}
+
+func (e GenerateError) Error() string {
+	return fmt.Sprintf("generate error: %s", e.Message)
+}
+
+// DeleteError is an error during entry deletion.
+type DeleteError struct {
+	Message string
+}
+
+func (e DeleteError) Error() string {
+	return fmt.Sprintf("delete error: %s", e.Message)
+}
