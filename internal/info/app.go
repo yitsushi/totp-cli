37078aab3e3607641c39e943b1b850d9e3ea83dd
Efertone <efertone@pm.me>
2023-08-30 14:22:46 +0200

    it seems ldflags is not a list
    Signed-off-by: Efertone <efertone@pm.me>

--- internal/info/app.go
+++ internal/info/app.go
@@ -7,4 +7,4 @@ const AppRepoOwner string = "yitsushi"
 const AppName string = "totp-cli"
 
 // AppVersion defined current version of this application.
-const AppVersion string = "v1.5.1"
+const AppVersion string = "v1.5.3"
